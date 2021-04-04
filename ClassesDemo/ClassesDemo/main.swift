ef76f2b68e0099b0362e12fff35fd7bd44319091
Chandran, Sudha <sudha.chandran@rakuten.com>
2021-04-04 16:01:52 +0530

    Optional Demo

--- ClassesDemo/ClassesDemo/main.swift
+++ ClassesDemo/ClassesDemo/main.swift
@@ -9,9 +9,48 @@ import Foundation
 
 let skeleton = Enemy(health: 10, attachStrength: 100)
 
+//Drogon child Class
 //let dragon = Dragon()
 //dragon.wingSpan = 5
 //dragon.attachStrength = 15
 //dragon.talk(speech: "asdksajkdhsajkbd akjlsdhasjkd laskjdhsjhdbjsh asjkdhskjdhs ")
 //dragon.move()
 //dragon.at tack()
+
+//Optional Demo
+
+let myOptional: String?
+
+myOptional = nil
+
+//1. Force Unwrapping
+//print(myOptional!)
+
+//2. Checking for Nil Value
+if myOptional != nil {
+    
+    print(myOptional!)
+}
+//3. Optional Binding
+if let test = myOptional {
+    print(test)
+}
+//4. Nil coalescing operator
+let printVal = myOptional ?? "Sudha"
+print(printVal)
+
+//opitional Struct / Class
+
+struct OptionalStruct {
+    var property = 123
+    func method() {
+        print("Im struct's method")
+    }
+}
+
+let myOptionalStruct: OptionalStruct?
+
+myOptionalStruct = OptionalStruct() //if nil below lines will crash
+
+print(myOptionalStruct!.method()) //optional chaining
+print(String(myOptionalStruct!.property))
